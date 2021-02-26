6819ddc2a246df87e3def398ac4e60a5130dcd6e
jean.santos <jean.santos@bancointer.com.br>
2021-02-25 21:17:27 -0300

    Initial commit

---
+++ SwiftHeroes/SwiftHeroes/SwiftHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHeroesApp.swift
+//  SwiftHeroes
+//
+//  Created by Jean Carlos Antônio Pereira dos Santos on 25/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
